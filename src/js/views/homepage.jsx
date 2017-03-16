abff58952a542258a4c7a9066acd6c1155f8430e
Chanse Campbell <chansecampbell@gmail.com>
2017-03-16 20:54:40 +0000

    Added a video detail page that will act as a show component for the selected video

--- src/js/views/homepage.jsx
+++ src/js/views/homepage.jsx
@@ -2,20 +2,24 @@
 import React from 'react';
 import Navbar from '../components/navbar.jsx';
 import VideoPreview from '../components/video-preview.jsx';
+import VideoDetail from '../components/video-detail.jsx';
+// import videoData from '../services/data.js';
 
 class HomepageView extends React.Component {
     constructor(props) {
         super(props);
+        // this.data = videoData.getAll();
     }   
 
     render() {
+        // console.log(this.data);
         return (
             <div className="homepage">
                 <Navbar/>
 
                 <div className="body container">
                     <VideoPreview />
-                </div>
+                </div> 
             </div>
         );
     }
