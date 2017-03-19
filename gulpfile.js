213b9125f5e4f43f8af8d12ca9afd02008d7ef2f
Chanse Campbell <chansecampbell@gmail.com>
2017-03-19 17:02:04 +0000

    Gulp now minifies the css and js and the html is using these files instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,38 @@
-const gulp      = require("gulp");
-const sass      = require("gulp-sass");
-const rename    = require("gulp-rename");
-const webpack   = require('webpack-stream');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const webpack = require('webpack-stream');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
-// grabs all of scss code and turns it into a singular css file
+// compiles the SASS code and creates both a minified and unminifed file
 gulp.task("sass", () => {
     return gulp.src("./src/css/styles.scss")
         .pipe(sass())
         .pipe(rename("bundle.css"))
-        .pipe(gulp.dest("./dist/css"));
+        .pipe(gulp.dest("./dist/css"))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename("bundle.min.css"))
+        .pipe(gulp.dest('./dist/css'));
 });
 
-
-// grabs all of the js code and using the webpack config turns it into a singular js file
+// compiles JS code and creates both a minified and unminified file
 gulp.task('js', () => {
     return gulp.src('./src/js/index.js')
         .pipe(webpack( require('./webpack.config.js') ))
         .pipe(rename("bundle.js"))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(uglify())
+        .pipe(rename("bundle.min.js"))
         .pipe(gulp.dest('./dist/js'));
 });
 
-
-// watches for any local changes and rebuilds
+// watches for any local changes and rebuilds the JS or CSS
 gulp.task('watch', () => {
     gulp.watch(['./src/**/*.js', './src/**/*.jsx'], ['js']);
     gulp.watch(['./src/**/*.css', './src/**/*.scss'], ['sass'])
 });
 
 
-// only needs to be run once upon first use of code to build the initial dist files
+// only needs to be run once upon first use of code to build the initial dist folder + files
 gulp.task('build', ['js', 'sass']);
